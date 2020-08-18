632bb14e7f3f36365678ac85bc66162164d6bd91
Phuong Le <phuonganh.le0312@gmail.com>
2020-08-18 00:11:27 -0400

    fixed typos and ran npm again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
 import Home from "./containers/Home";
 
 const App = () => {
